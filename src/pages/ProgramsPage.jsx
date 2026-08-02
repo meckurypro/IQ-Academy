7f198bd0efd3e568debde8282e32dd753a2c27cb
meckurypro <meckurypro@gmail.com>
2026-08-02 22:59:23 +0100

    Create ProgramsPage component to display Programs

---
+++ src/pages/ProgramsPage.jsx
@@ -0,0 +1,11 @@
+// src/pages/ProgramsPage.jsx
+import Programs from '../components/Programs'
+
+export default function ProgramsPage() {
+  return (
+    <div style={{ paddingTop: 40 }}>
+      <Programs />
+    </div>
+  )
+}
+
