66037e29d81563aa6f0c5a0d28f682b879194f69
meckurypro <meckurypro@gmail.com>
2026-08-02 22:52:00 +0100

    Create main App component with routing

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+// src/App.jsx
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import ProtectedStaffRoute from './components/ProtectedStaffRoute'
+
+import Home from './pages/Home'
+import ProgramsPage from './pages/ProgramsPage'
+import TrainingsPage from './pages/TrainingsPage'
+import GalleryPage from './pages/GalleryPage'
+import StaffLogin from './pages/staff/StaffLogin'
+import StaffDashboard from './pages/staff/StaffDashboard'
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/programs" element={<ProgramsPage />} />
+          <Route path="/trainings" element={<TrainingsPage />} />
+          <Route path="/gallery" element={<GalleryPage />} />
+          <Route path="/staff" element={<StaffLogin />} />
+          <Route
+            path="/staff/dashboard"
+            element={
+              <ProtectedStaffRoute>
+                <StaffDashboard />
+              </ProtectedStaffRoute>
+            }
+          />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </AuthProvider>
+  )
+}
