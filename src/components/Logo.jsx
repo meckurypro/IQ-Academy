25700d5dababbb7c16ed88ad2d72b5a729df92a7
meckurypro <meckurypro@gmail.com>
2026-08-11 19:46:07 +0100

    Update Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,5 @@
 // src/components/Logo.jsx
-const LOGO_URL = 'https://raw.githubusercontent.com/meckurypro/PromptIQ-/main/public/iqacademy.png'
+const LOGO_URL = '/academyfav.png'
 
 export default function Logo({ className = '' }) {
   return <img src={LOGO_URL} alt="IQ Academy" className={`logo-mark ${className}`.trim()} />
